0dcb01357a9ce1e801dba06a77c0b13e0ea34990
Kratious <semjinghan@gmail.com>
2021-01-12 01:09:02 +0800

    Add linting and autoformatting rules

---
+++ README.md
@@ -0,0 +1,11 @@
+# Setup venv
+
+
+Install venv (Linux):
+`apt install python3-venv`
+
+Create venv folder:
+`python3 -m venv venv`
+
+Activate venv:
+`source venv/bin activate`
--- bot.py
+++ bot.py
@@ -5,20 +5,23 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from datetime import date
 
 from credentials import TOKEN, URL
-from claim import *
+from claim import Claim
 
 # Enable logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
 
 logger = logging.getLogger(__name__)
 
-PORT = int(os.environ.get('PORT', '8443'))
+PORT = int(os.environ.get("PORT", "8443"))
 
 global chats
 chats = {}
 
 # Ordinary commands to appear in telegram
+
+
 def start(update, context):
     global chats
 
@@ -27,6 +30,7 @@ def start(update, context):
     chats[chat_id] = []
 
     general_message(update)
+
 
 def add(update, context):
     global chats
@@ -38,14 +42,22 @@ def add(update, context):
         send_reply(update, "Please create new claim in the form of <NAME> <AMT>")
     else:
         last_index = len(args)
-        if(is_number(args[last_index - 1]) and float(args[last_index - 1]) > 0):
-            claim_name = " ".join(args[:last_index - 1])
+        if is_number(args[last_index - 1]) and float(args[last_index - 1]) > 0:
+            claim_name = " ".join(args[: last_index - 1])
             claim_amt = float(args[last_index - 1])
             claim = Claim(claim_name, claim_amt, date.today())
             chats[chat_id].append(claim)
-            send_reply(update, "Ok, claim amount of *$" + get_currency(claim_amt) + "* for *" + claim_name + "* has been created")
+            send_reply(
+                update,
+                "Ok, claim amount of *$"
+                + get_currency(claim_amt)
+                + "* for *"
+                + claim_name
+                + "* has been created",
+            )
         else:
             send_reply(update, "Please enter a valid amount more than $0")
+
 
 def get(update, context):
     chat_id = update.message.chat.id
@@ -54,12 +66,22 @@ def get(update, context):
     index = 1
 
     for c in chats[chat_id]:
-        msg += str(index) + ". " + c.name + " (" + c.date.strftime("%d/%m/%Y") + ") - $" + get_currency(c.amount) + "\n"
-        index += 1 
+        msg += (
+            str(index)
+            + ". "
+            + c.name
+            + " ("
+            + c.date.strftime("%d/%m/%Y")
+            + ") - $"
+            + get_currency(c.amount)
+            + "\n"
+        )
+        index += 1
 
-    header_msg = "You have " + str(len(chats[chat_id])) + " open claim(s). \n\n"        
+    header_msg = "You have " + str(len(chats[chat_id])) + " open claim(s). \n\n"
     send_reply(update, header_msg + msg)
-    
+
+
 def close(update, context):
     global chats
     chat_id = update.message.chat.id
@@ -71,23 +93,29 @@ def close(update, context):
         index = int(args[0])
         if valid_range(index, chats[chat_id]):
             c = chats[chat_id].pop(index - 1)
-            send_reply(update, "You have closed a claim of *$" + get_currency(c.amount) + "*")
+            send_reply(
+                update, "You have closed a claim of *$" + get_currency(c.amount) + "*"
+            )
             get(update, context)
         else:
-            send_reply(update, "Invalid claim index. Please check available claims using /list")
+            send_reply(
+                update, "Invalid claim index. Please check available claims using /list"
+            )
+
 
 def restart(update, context):
     global chats
     chat_id = update.message.chat.id
-    
+
     chats[chat_id] = []
 
     send_reply(update, "I have cleared all claims")
 
 
-# Helper methods    
+# Helper methods
 def send_reply(update, msg):
-    update.message.reply_text(msg, parse_mode= 'Markdown')
+    update.message.reply_text(msg, parse_mode="Markdown")
+
 
 def is_number(s):
     try:
@@ -96,6 +124,7 @@ def is_number(s):
     except ValueError:
         return False
 
+
 def valid_range(value, lst):
     length = len(lst)
     if value >= 1 and value <= length:
@@ -103,17 +132,26 @@ def valid_range(value, lst):
     else:
         return False
 
+
 def get_currency(amt):
     return "{0:,.2f}".format(amt)
 
+
 # Others
+
+
 def general_message(update):
-    send_reply(update, 'Hello, welcome to the Claims Tracker Bot! \nYou can use me to keep track of any claims you may have~ \n\nStart by making a new claim with /add')
+    send_reply(
+        update,
+        "Hello, welcome to the Claims Tracker Bot! \nYou can use me to keep track of any claims you may have~ \n\nStart by making a new claim with /add",
+    )
+
 
 def error(update, context):
     """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, context.error)
     command = 0
+
 
 def main():
     """Start the bot."""
@@ -133,7 +171,7 @@ def main():
     dp.add_handler(CommandHandler("restart", restart))
 
     # on noncommand i.e message - echo the message on Telegram
-    #dp.add_handler(MessageHandler(Filters.text, handle_commands))
+    # dp.add_handler(MessageHandler(Filters.text, handle_commands))
 
     # log all errors
     dp.add_error_handler(error)
@@ -148,5 +186,5 @@ def main():
     updater.idle()
 
 
-if __name__ == '__main__':
-    main() 
+if __name__ == "__main__":
+    main()
