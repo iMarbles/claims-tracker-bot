96b8f808eeaef63f2743fadd982716a6a288ec90
Amabel <mabel_yeo@hotmail.com>
2021-01-11 21:44:22 +0800

    formatting

--- bot.py
+++ bot.py
@@ -137,12 +137,8 @@ def main():
     # updater.bot.set_webhook(URL + TOKEN)
     # updater.idle()
 
-     # Start the Bot (LOCALHOST)
+    # Start the Bot (LOCALHOST)
     updater.start_polling()
-
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
 
 
